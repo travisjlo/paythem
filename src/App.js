43c5316c4652a08bc150dd96b7f2fe7100f174b7
Travis Lo <TJlo1@asu.edu>
2018-11-10 16:40:54 -0700

    fixed amount

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import logo from './payThemlogo.png';
 import './App.css';
+import CheckoutForm from './CheckoutForm';
+import {Elements, StripeProvider} from 'react-stripe-elements';
 
 class App extends Component {
+  
   render() {
+    var message2 = " subway sucks jk";
     return (
       <div className="App">
         <header className="App-header">
@@ -19,8 +23,19 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React
+
           </a>
         </header>
+
+        <StripeProvider apiKey="pk_test_5K9SYJEzAULsJU0SKgSVm8r1">
+          <div className="example">
+            <h1>React Stripe Elements Example</h1>
+            <Elements>
+              <CheckoutForm message = {"send" }/>
+            </Elements>
+          </div>
+        </StripeProvider>
+        
       </div>
     );
   }
---
+++ src/CheckoutForm.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {CardElement, injectStripe} from 'react-stripe-elements';
+
+class CheckoutForm extends Component {
+  constructor(props) {
+    super(props);
+    this.submit = this.submit.bind(this);
+    this.state = { message: props.message, style: "#000", amount: 10};
+    this.validate = this.validate.bind(this);
+  }
+
+  handleChange(event){
+      this.setState({amount:event.target.value});
+  }
+
+  async submit(ev) {
+    // User clicked submit
+    this.setState({message:"Gotcha ", style: "#fff"});
+    let {token} = await this.props.stripe.createToken({name: "Name"});
+    let response = await fetch("/charge", {
+        method: "POST",
+        headers: {"Content-Type": "text/plain"},
+        body: {id:token.id, amount: this.state.amount} 
+    });
+    if (response.ok) this.setState({complete: true});
+    else {this.setState({message: "brah thats not a real card"})};
+  }
+
+  render() {
+    if (this.state.complete) return <h1>Purchase Complete</h1>;
+
+    return (
+      <div className="checkout">
+        <p> Click the black button!</p>
+        <CardElement/>
+        <input type='text' onKeyPress={this.validate} value={this.state.amount} onChange={this.handleChange}></input>
+     
+        <button style = {{backgroundColor: this.state.style}} onClick={this.submit}>{this.state.message}</button>
+      </div>
+    );
+  }
+  validate(evt) {
+        var theEvent = evt || window.event;
+    
+        // Handle paste
+    
+        // Handle key press
+            var key = theEvent.keyCode || theEvent.which;
+            key = String.fromCharCode(key);
+        
+        var regex = /[0-9]|\./;
+        if( !regex.test(key) ) {
+        theEvent.returnValue = false;
+        if(theEvent.preventDefault) theEvent.preventDefault();
+        }   
+    }
+}
+
+export default injectStripe(CheckoutForm);
+//export default CheckoutForm;

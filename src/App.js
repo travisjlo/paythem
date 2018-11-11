5cdf8c61cfb9f63e1babd95ea326e6a915676620
Travis Lo <TJlo1@asu.edu>
2018-11-10 17:09:24 -0700

    minor fixes

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-            helllllllllllllo
+            send us money!
           </p>
           <a
             className="App-link"
@@ -22,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            
 
           </a>
         </header>
